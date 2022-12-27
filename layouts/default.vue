f4ad9f547ac970e6ffc83f922fb7caa245d34f2f
HugoRCD <hugo.richard@epitech.eu>
2022-12-27 13:49:07 +0100

    Add default Layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script>
+import {defineComponent} from "vue";
+import Navbar from "~/components/layout/Navbar.vue";
+
+export default defineComponent({
+  components: {Navbar}
+})
+
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
