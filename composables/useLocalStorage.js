ee02f36b45fbccf2f200661767c7839e4fe695b7
HugoRCD <hugo.richard@epitech.eu>
2023-01-02 10:05:20 +0100

    Update CI/CD

--- composables/useLocalStorage.js
+++ composables/useLocalStorage.js
@@ -1,12 +1,12 @@
 export const useLocalStorage = () => {
   function set(key, value) {
-    if (process.client) localStorage.setItem(key, value);
+    localStorage.setItem(key, value);
   }
   function get(key) {
-    if (process.client) return localStorage.getItem(key);
+    return localStorage.getItem(key);
   }
   function remove(key) {
-    if (process.client) localStorage.removeItem(key);
+    localStorage.removeItem(key);
   }
   return {
     set,
