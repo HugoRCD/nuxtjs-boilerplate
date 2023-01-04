fb758a960b53d81b2afbc1f2f9e440b8934fee29
HugoRCD <hugo.richard@epitech.eu>
2023-01-04 16:46:47 +0100

    Change for ssr false

--- plugins/google-auth.ts
+++ plugins/google-auth.ts
@@ -1,6 +1,8 @@
 import vue3GoogleLogin from "vue3-google-login";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(vue3GoogleLogin, {
-    clientId: useRuntimeConfig().public.googleClientId,
+    clientId:
+      useRuntimeConfig().public.googleClientId ||
+      import.meta.env.VITE_PUBLIC_GOOGLE_CLIENT_ID,
   });
 });
