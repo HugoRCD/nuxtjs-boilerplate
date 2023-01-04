f8b866239762c5c6d6f28d997fe7549f5b4e3d67
HugoRCD <hugo.richard@epitech.eu>
2023-01-04 19:00:36 +0100

    test something

--- plugins/google-auth.ts
+++ plugins/google-auth.ts
@@ -1,8 +1,6 @@
 import vue3GoogleLogin from "vue3-google-login";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(vue3GoogleLogin, {
-    clientId:
-      useRuntimeConfig().public.googleClientId ||
-      import.meta.env.VITE_PUBLIC_GOOGLE_CLIENT_ID,
+    clientId: import.meta.env.VITE_PUBLIC_GOOGLE_CLIENT_ID,
   });
 });
