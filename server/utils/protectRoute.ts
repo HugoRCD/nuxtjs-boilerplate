6c3e22612cd32875c63a534814ff716b4fb3bce9
HugoRCD <hugo.richard@epitech.eu>
2023-01-25 00:12:22 +0100

    Update Eslint

--- server/utils/protectRoute.ts
+++ server/utils/protectRoute.ts
@@ -1,10 +1,10 @@
-import { H3Event } from 'h3';
+import { H3Event } from "h3";
 
 export default (event: H3Event) => {
   if (!event.context.user) {
     throw createError({
       statusCode: 401,
-      message: 'Unauthorized',
+      message: "Unauthorized",
     });
   }
 };
