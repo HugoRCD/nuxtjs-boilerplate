1bb9dc013e761c358fd0dbdaa8cf6b6ad0362d32
HugoRCD <hugo.richard@epitech.eu>
2023-01-24 16:39:06 +0100

    Add prisma client

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
--- server/api/user/currentUser.get.ts
+++ server/api/user/currentUser.get.ts
@@ -1,5 +1,7 @@
 import protectRoute from "~/server/utils/protectRoute";
 import { serverSupabaseUser } from "#supabase/server";
+/* import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient(); */
 
 export default defineEventHandler(async (event) => {
   protectRoute(event);
