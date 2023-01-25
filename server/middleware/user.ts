4ee16b81008d7a5ce7de47e4b83036498f421a6b
HugoRCD <hugo.richard@epitech.eu>
2023-01-25 15:48:39 +0100

    Fix bug

--- server/middleware/user.ts
+++ server/middleware/user.ts
@@ -3,6 +3,9 @@ import { serverSupabaseClient, serverSupabaseUser } from "#supabase/server";
 export default defineEventHandler(async (event) => {
   const client = serverSupabaseClient(event);
   event.context.user = await serverSupabaseUser(event);
+  if (!event.context.user) {
+    return;
+  }
   const { data } = await client.from("account").select("*").eq("id", event.context.user.id).single();
   if (data) {
     event.context.user.admin = data.admin;
